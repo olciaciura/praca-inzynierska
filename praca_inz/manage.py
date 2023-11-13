c1231acc1bec9f798d805f86cb80f798f86fe4bd
aleciura <aleksandra.ciura@capgemini.com>
2023-11-13 14:49:43 +0100

    preprocess, load, predict function

--- praca_inz/manage.py
+++ praca_inz/manage.py
@@ -2,11 +2,14 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+from model.utils import load_model, predict
 
+MODEL = load_model('./model/model.p')
 
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'praca_inz.settings')
+    predict(MODEL, './model/photos/test/1.jpg')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -16,8 +19,8 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
-    
-    #TODO dodać ładowanie modelu  do zmiennej globalnej 
+
+    #TODO dodać ładowanie modelu  do zmiennej globalnej
 
 if __name__ == '__main__':
     main()
--- praca_inz/praca_inz/settings.py
+++ praca_inz/praca_inz/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-_o96w8ft0utkkel%lratcwq7qa#xxsvfscq1-ashxk6=iqp6%w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]']
 
 
 # Application definition
@@ -119,8 +119,9 @@ USE_TZ = True
 
 STATIC_URL ='/static/'
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+     "static",
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
