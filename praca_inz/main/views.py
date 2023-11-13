0c16587275c08ba00b071f16a5801a5612502b26
aleciura <aleksandra.ciura@capgemini.com>
2023-11-13 21:17:41 +0100

    working app <3

--- praca_inz/main/views.py
+++ praca_inz/main/views.py
@@ -5,7 +5,8 @@ from django.template import loader
 
 from .models import DataModel
 from .utils import draw_frame_on_image, clear_media_folder
-
+from ..model.utils import predict
+from ..manage import MODEL
 # Create your views here.
 
 # klasa ImageUploadView - w tym metoda get - pobieranie zdjecia, post - przetworzenie zdjecia i wysłanie z ramką
@@ -25,12 +26,11 @@ def model(request):
 
     last_entry = DataModel.objects.last()
 
-# TODO: należy coordynaty policzyc z modelu a nie podać z ręki
-
-    coordinates = { 'left':     50,
-                    'right':    100,
-                    'bottom':   100,
-                    'top':      50}
+    coordinates = predict(MODEL, last_entry)
+    # coordinates = { 'left':     50,
+    #                 'right':    100,
+    #                 'bottom':   100,
+    #                 'top':      50}
 
     draw_frame_on_image(last_entry.file if last_entry else None, coordinates)
 
